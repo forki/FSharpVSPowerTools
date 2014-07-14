b844cd65c7520bccbb6ef277a7ed760ba9222b81
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-07-14 11:01:25 +0200

    Create a rename suggestion list
      - spell checking
      - thesaurus

---
+++ src/FSharpVSPowerTools.Logic/RenameSuggestions.fs
@@ -0,0 +1,89 @@
+﻿module FSharpVSPowerTools.Refactoring.RenameSuggestions
+
+open System.Text.RegularExpressions
+open System
+open NHunspell
+
+type Kind =
+| Type
+| Variable
+
+let suggestIndexNames name =
+    let indexRegex = Regex("[i](\d*)")
+    [ let indexMatch = indexRegex.Match(name)
+      if indexMatch.Success then
+          yield "index" + indexMatch.Groups.[1].Value
+      if name = "index" then
+          yield "i"
+      if name = "i2" || name = "index2" then
+          yield "j"
+      if name = "i3" || name = "index3" then
+          yield "k" ]
+
+let lower (s:string) =
+    s.[0].ToString().ToLower() + if s.Length > 1 then s.Substring(1) else ""
+
+let upper (s:string) =
+    s.[0].ToString().ToUpper() + if s.Length > 1 then s.Substring(1) else ""
+
+let splitInParts (name:string) =
+    [let last = ref 0
+     for i in 0..name.Length-1 do
+        if Char.IsUpper(name.[i]) && i <> 0 then
+            yield name.Substring(!last,i - !last)
+            last := i
+        if i = name.Length - 1 then
+            yield name.Substring(!last)]
+
+let internal thes = lazy(new MyThes("th_en_us_new.dat"))
+
+let findSynonyms hunspell word =
+    let thes = thes.Force()
+    let tr = thes.Lookup(word, hunspell)
+
+    [if tr <> null && tr.Meanings <> null then
+        for meaning in tr.Meanings do
+            if meaning.Synonyms <> null then
+                for s in meaning.Synonyms do
+                    if s <> word && not (s.Contains " ") && not (s.Contains "-") then                                     
+                        if Char.IsLower(word.[0]) then yield lower s else yield upper s]
+    |> Set.ofList
+
+
+let createSuggestions name =    
+    use hunspell = new Hunspell("en_us.aff", "en_us.dic")
+    let parts = splitInParts name
+
+    let rec loop parts =
+        match parts with
+        | [] -> []
+        | x::rest ->
+            let laterParts = loop rest
+            let prepend element =
+                [if laterParts = [] then
+                     yield [element]
+                 for l in laterParts do
+                     yield element :: l]
+
+            if hunspell.Spell x then
+                // word is correct - try to find synonyms
+                [yield! prepend x
+
+                 for w in findSynonyms hunspell x do
+                     yield! prepend w]
+            else
+                // word is not written correctly try to correct
+                [for s in hunspell.Suggest x do
+                     yield! prepend s]
+
+    loop parts
+    |> List.map String.Concat
+
+let suggest (kind:Kind) (name:string) =    
+    match kind with
+    | Variable -> 
+        lower name ::
+        suggestIndexNames name
+    | Type -> [upper name]
+    |> List.append (createSuggestions name)
+    |> List.filter ((<>) name)    
---
+++ tests/FSharpVSPowerTools.Logic.Tests/RenameSuggestionTests.fs
@@ -0,0 +1,82 @@
+﻿module FSharpVSPowerTools.Logic.Tests.RenameSuggestionTests
+
+open NUnit.Framework
+open FSharpVSPowerTools.Refactoring.RenameSuggestions
+
+[<Test>]
+let ``should suggest index names``() = 
+    suggest Kind.Variable "i" |> shouldContain "index"
+    suggest Kind.Variable "i2" |> shouldContain "index2"    
+    suggest Kind.Variable "i2452" |> shouldContain "index2452"
+    suggest Kind.Variable "i2" |> shouldContain "j"
+    suggest Kind.Variable "i3" |> shouldContain "k"
+    suggest Kind.Variable "index" |> shouldContain "i"
+    suggest Kind.Variable "index2" |> shouldContain "j"    
+    suggest Kind.Variable "index3" |> shouldContain "k"
+
+[<Test>]
+let ``should suggest lower case variable names``() = 
+    suggest Kind.Variable "I" |> shouldContain "i"
+    suggest Kind.Variable "Index" |> shouldContain "index"    
+    suggest Kind.Variable "MyVariable" |> shouldContain "myVariable"
+
+[<Test>]
+let ``should not suggest lower case type names``() = 
+    suggest Kind.Type "I" |> shouldNotContain "i"
+    suggest Kind.Type "Index" |> shouldNotContain "index"    
+    suggest Kind.Type "MyVariable" |> shouldNotContain "myVariable"
+
+[<Test>]
+let ``should suggest upper case types``() = 
+    suggest Kind.Type "myClass" |> shouldContain "MyClass"
+    suggest Kind.Type "book" |> shouldContain "Book"
+    suggest Kind.Type "bookIndex" |> shouldContain "BookIndex"
+
+[<Test>]
+let ``should not suggest upper case variable names``() = 
+    suggest Kind.Variable "i" |> shouldNotContain "I"
+    suggest Kind.Variable "index" |> shouldNotContain "Index"    
+    suggest Kind.Variable "myVariable" |> shouldNotContain "MyVariable"
+
+[<Test>]
+let ``should correct simple type names``() =    
+    suggest Kind.Type "Recommendatio" |> shouldContain "Recommendation"
+    suggest Kind.Type "Houlse" |> shouldContain "House"
+
+[<Test>]
+let ``should correct simple function names``() =    
+    suggest Kind.Variable "reccomend" |> shouldContain "recommend"
+    suggest Kind.Variable "annalyze" |> shouldContain "analyze"
+
+[<Test>]
+let ``should split longer names``() =    
+    splitInParts "" |> Seq.isEmpty |> assertTrue 
+    splitInParts "RecommendationEngine" |> assertEqual ["Recommendation"; "Engine"]
+    splitInParts "HouseOfCards" |> assertEqual ["House"; "Of"; "Cards"]
+    splitInParts "RecommenA" |> assertEqual ["Recommen"; "A"]
+    splitInParts "analyzeFunctionName" |> assertEqual ["analyze"; "Function"; "Name"]
+
+[<Test>]
+let ``should correct longer type names``() =    
+    suggest Kind.Type "RecommendatioEngine" |> shouldContain "RecommendationEngine"
+    suggest Kind.Type "HoulseOfCards" |> shouldContain "HouseOfCards"
+
+[<Test>]
+let ``should create suggestions for function names``() =    
+    createSuggestions "annalyze" |> assertEqual ["analyze"; "analyzable"]
+    createSuggestions "anallyzeFunctionName" |> shouldContain "analyzeFunctionName"
+    createSuggestions "anallyzeFunctionName" |> shouldContain "analyzableFunctionName"
+    createSuggestions "getHoulseOfCards" |> shouldContain "getHouseOfCards"
+
+[<Test>]
+let ``should correct longer function names``() =    
+    suggest Kind.Variable "getMyFrstParamter" |> shouldContain "getMyFirstParameter"
+    suggest Kind.Variable "whereIsMyHoulseOfCards" |> shouldContain "whereIsMyHouseOfCards"
+
+[<Test>]
+let ``should suggest synonyms for simple types``() =
+    suggest Kind.Type "Parameter" |> shouldContain "Constant"
+    suggest Kind.Type "Parameter" |> shouldNotContain "Parameter"
+    suggest Kind.Type "Parameter" |> shouldContain "Factor"    
+    suggest Kind.Type "analyze" |> shouldContain "study"
+    suggest Kind.Type "Basket" |> shouldContain "Container"
